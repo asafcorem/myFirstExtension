a9eb2e5b0412d0b7d0a974ffb444643ebeb39d75
acorem <ACorem@agtinternational.com>
2015-12-04 18:38:03 +0200

    before start scaning

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<pk>[0-9]+)/posts_display/$', views.IndexView.as_view(), name='posts_display'),
     url(r'^1/clean_data_bases/$', views.clean_data_bases, name='clean_data_bases'),
@@ -11,6 +10,7 @@ urlpatterns = [
     url(r'^(?P<user_id>[0-9]+)/saving/$', views.saving, name='saving'),
     url(r'^([0-9]+)/fsdssd/$', views.start_scan, name='start_scan'),
     url(r'^([0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
 #     url(r'^homepage/$', views.DetailView.as_view(), name='asdfsa'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,35 +1,81 @@
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect , HttpResponse
-from django.core.urlresolvers import reverse
-from django.views import generic
-from django.contrib.auth.models import User
- 
-from .models import Choice, Question ,Groups
-from myFirstExtension import phone_from_facebook
-import threading
-from lib2to3.fixer_util import is_import
-import time
-from polls.models import Posts,Scans
-from datetime import date
-import calendar
-from django.shortcuts import redirect
 import bz2
+import calendar
+from datetime import date
+from lib2to3.fixer_util import is_import
+import threading
+import time
+
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect , HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.shortcuts import redirect
+from django.views import generic
+from django.views.generic import DetailView
+
+from myFirstExtension import phone_from_facebook
+from polls.models import Posts, Scans
+
+from .models import Choice, Question , Groups
 
 
 class IndexView(generic.DetailView):
-    model = User
+    print 3333333333
     template_name = 'polls/index.html'
- 
+    print 4545454545 
+    def get_object(self):
+        print 555555555
+        print "army is " + str(self.kwargs['pk'])
+        user_by_the_url = get_object_or_404(User, pk=self.kwargs['pk'])
+        user_by_session =  self.request.user
+        print user_by_the_url
+        print user_by_session
+        print self.request.user.is_authenticated()
+        if user_by_the_url == user_by_session:
+            return user_by_the_url
+        else:
+            return get_object_or_404(User, pk="1111")
+        
+        
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        print 444444
+        context = super(IndexView, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['book_list'] = "blblblblbl"
+        return context        
+        
+    
 class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
+    print "nnnnnnn"
     
     def get_object(self):
-        new_user , created = User.objects.get_or_create(username="sadsadasd", password="asds")
-        return get_object_or_404(User, pk=new_user.id)
+        print "we here 6"
+        IndexView()
+        if self.request.user.is_authenticated():
+            return self.request.user
+        else:    
+            return None
  
+    def get_template_names(self):
+        if self.request.user.is_authenticated():
+            return 'polls/index.html'
+        else:    
+            return 'polls/detail.html'
+        
+        
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        print 444444
+        context = super(DetailView, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['last_scan'] = Scans.objects.order_by('-id')[0]
+        return context    
  
 class ResultsView(generic.DetailView):
+    print "we here 5"
     model = User
     template_name = 'polls/results.html'
  
@@ -37,16 +83,19 @@ def vote(request,asdsad):
     user_email =  request.POST['email']
     user_password =  request.POST['password'].encode('utf-8')
     
-    FB_phone =  phone_from_facebook.facebook_phone(user_email , user_password)
-    list_of_all_groups = FB_phone.get_all_group_of_user()
-    user = User.objects.create_user(password = user_password , username = user_email ,first_name = user_password)
-    if len(list_of_all_groups)>0:           
-        user.save()
-        for group in list_of_all_groups: 
-            new_group = Groups(group_name = group["name"] ,group_link = group["link"], user_own = user)
-            new_group.save()  
-                       
-    return HttpResponseRedirect(reverse('polls:results', args=(user.id,)))
+    user = authenticate(username=user_email, password=user_password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse('polls:posts_display', args=(user.id,)))  
+    else:
+        print "we here"
+        new_user_register(user_email , user_password)
+        print "we here 2"
+        new_user = authenticate(username=user_email, password=user_password)
+        login(request, new_user)
+        print "we here 3"
+        return HttpResponseRedirect(reverse('polls:results', args=(new_user.id,)))
+
      
 def saving(request, user_id):
     all_checked_groups = request.POST.getlist("group_to_mark")
@@ -59,10 +108,8 @@ def saving(request, user_id):
         p.keyword = keyword
         p.save()
     first_scan_for_user(user_id)
-    a = User.objects.get(id = user_id)
-    print a
-    print a.id
-    return HttpResponseRedirect(reverse('polls:posts_display', args=(a.id,)))  
+    user = User.objects.get(id = user_id)
+    return HttpResponseRedirect(reverse('polls:posts_display', args=(user.id,)))  
 
 def scan_thread():
     while(True):
@@ -111,3 +158,17 @@ def clean_data_bases(request):
     Groups.objects.all().delete()
     Posts.objects.all().delete()
     return HttpResponse("we cleaned all") 
+
+
+def new_user_register(user_email , user_password):
+        FB_phone =  phone_from_facebook.facebook_phone(user_email , user_password)
+        list_of_all_groups = FB_phone.get_all_group_of_user()
+        user = User.objects.create_user(password = user_password , username = user_email ,first_name = user_password)
+        if len(list_of_all_groups)>0:           
+            user.save()
+            for group in list_of_all_groups: 
+                new_group = Groups(group_name = group["name"] ,group_link = group["link"], user_own = user)
+                new_group.save()  
+        return user             
+
+    
